f22896012acc255eb48d8f4c11b1fde89677d062
yuzhiyuan <yuzhiyuan@ijunhai.com>
2019-03-27 11:51:48 +0800

    修正

--- kubernetes_build_up/helm.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p /kubernetes/helm ; cd /kubernetes/helm
-tar -xf /root/helm-v2.12.1-linux-amd64.tar.gz -C .
-rm -f /usr/local/bin/helm ; cp linux-amd64/helm /usr/local/bin/
-helm init --upgrade -i registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.12.1 --stable-repo-url https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts
-helm version
